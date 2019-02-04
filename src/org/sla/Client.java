d6b88cf7abb0337bd70d5c8a6ddc6884f631a7e0
vhernandez <vhernandez@scienceleadership.org>
2019-02-04 12:03:50 -0500

    Switch to using ObjectOutputStream/ObjectInputStream for data write/read

--- src/org/sla/Client.java
+++ src/org/sla/Client.java
@@ -8,28 +8,45 @@ public class Client {
     public static void main(String[] args) {
         try {
             // Connect to a server that is listening at IP address 127.0.0.1 (the same computer) on port 5000
-            Socket serverSocket = new Socket("127.0.0.1", 5000);
+            Socket socketClientSide = new Socket("127.0.0.1", 5000);
             System.out.println("Basic Client: connected to server at IP address 127.0.0.1 on port 5000");
 
-            // The serverSocket provides 2 separate streams for 2-way communication
-            // the InputStream is for communication FROM server TO client
-            InputStream communicationIn = serverSocket.getInputStream();
-            // the OutputStream is for communication TO server FROM client
-            OutputStream communicationOut = serverSocket.getOutputStream();
+            // The Socket provides 2 separate streams for 2-way communication:
+            //   the InputStream is for communication FROM server TO client
+            //   the OutputStream is for communication TO server FROM client
+            // Create data reader and writer from those streams (NOTE: ObjectOutputStream MUST be created FIRST)
+            ObjectOutputStream dataWriter = new ObjectOutputStream(socketClientSide.getOutputStream());
+            ObjectInputStream dataReader = new ObjectInputStream(socketClientSide.getInputStream());
 
-            // We're connected to server!  Let's say hi by writing to the output side of the socket.
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(communicationOut));
-            writer.write("HI FROM CLIENT #" + args[0] + "\n");
-            writer.flush();
-            System.out.println("Basic Client: SENT \"HI FROM CLIENT #" + args[0]+ "\"");
+            // We're connected to server!  Let's say hi.
+            // Let server know how much data is being sent
+            Integer dataCount = 3;
+            dataWriter.writeObject(dataCount);
+            dataWriter.flush();
+            System.out.println("Basic Client SENDING: " + dataCount + " data");
 
-            writer.write("HI AGAIN FROM CLIENT #" + args[0] + "\n");
-            writer.flush();
-            System.out.println("Basic Client: SENT \"HI AGAIN FROM CLIENT #" + args[0]+ "\"");
+            // Write 3 messages to output side of the socket
+            String data1 = "HI FROM CLIENT #" + args[0];
+            dataWriter.writeObject(data1);
+            dataWriter.flush();
+            System.out.println("Basic Client SENT: " + data1);
 
-            writer.write("WHAT'S UP FROM CLIENT #" + args[0] + "\n");
-            writer.flush();
-            System.out.println("Basic Client: SENT \"WHAT'S UP FROM CLIENT #" + args[0]+ "\"");
+            String data2 = "HI AGAIN FROM CLIENT #" + args[0];
+            dataWriter.writeObject(data2);
+            dataWriter.flush();
+            System.out.println("Basic Client SENT: " + data2);
+
+            String data3 = "WHAT'S UP FROM CLIENT #" + args[0];
+            dataWriter.writeObject(data3);
+            dataWriter.flush();
+            System.out.println("Basic Client SENT: " + data3);
+
+            // Done with communication: close sockets
+            System.out.println("Basic Client: closing sockets");
+            dataReader.close();
+            dataWriter.close();
+            socketClientSide.close();
+
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Basic Client: networking failed. Exiting....");
--- src/org/sla/Server.java
+++ src/org/sla/Server.java
@@ -7,28 +7,38 @@ public class Server {
 
     public static void main(String[] args) {
         // Set up server-side networking
+
         try {
-            // Start listening for client connections on port 5000
+            // Use a ServerSocket to start listening for client connections on port 5000
             ServerSocket connectionSocket = new ServerSocket(5000);
             System.out.println("Basic Server: server is listening on port 5000");
+
             // Wait until a client tries to connect
-            Socket clientSocket = connectionSocket.accept();
+            Socket socketServerSide = connectionSocket.accept();
             System.out.println("Basic Server: a client has connected!");
 
-            // The clientSocket provides 2 separate streams for 2-way communication
-            // the InputStream is for communication FROM client TO server
-            InputStream communicationIn = clientSocket.getInputStream();
-            // the OutputStream is for communication TO client FROM server
-            OutputStream communicationOut = clientSocket.getOutputStream();
-            // Create reader and writer from those streams
-            BufferedReader reader = new BufferedReader(new InputStreamReader(communicationIn));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(communicationOut));
+            // The Socket provides 2 separate streams for 2-way communication:
+            //   the InputStream is for communication FROM client TO server
+            //   the OutputStream is for communication TO client FROM server
+            // Create data reader and writer from those stream (NOTE: ObjectOutputStream MUST be created FIRST)
+            ObjectOutputStream dataWriter = new ObjectOutputStream(socketServerSide.getOutputStream());
+            ObjectInputStream dataReader = new ObjectInputStream(socketServerSide.getInputStream());
 
-            // A client has connected!  Let's read from the input side of the socket.
-            String message;
-            while ((message = reader.readLine()) != null) {
-                System.out.println("Basic Server: RECEIVE \"" + message + "\"");
+            // A client has connected!  Let's find out how much data it's sending
+            int dataCount = (Integer)dataReader.readObject();
+            System.out.println("Basic Server: RECEIVING " + dataCount + " data");
+            // Read that count of data from the input side of the socket.
+            for (int i = 0; i < dataCount; i = i + 1) {
+                Object data = dataReader.readObject();
+                System.out.println("Basic Server: RECEIVE \"" + data + "\"");
             }
+
+            // Done with communication: close sockets
+            System.out.println("Basic Server: closing sockets");
+            dataReader.close();
+            dataWriter.close();
+            socketServerSide.close();
+            connectionSocket.close();
 
         } catch (Exception ex) {
             ex.printStackTrace();
